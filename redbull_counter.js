d22d80e5c2b950ee1d2c58fb0e2ba39c052ceae6
System Administrator <root@ayananote.local>
2015-03-21 01:03:40 +0900

    結構変更

--- redbull_counter.js
+++ redbull_counter.js
@@ -12,12 +12,15 @@ rc = (function(){
 	var classRedbull = "redbull";
 	var classClosed = "redbull_closed";
 	var classOpen = "redbull_open";
+	var classFocus = "redbull_highlight";
 
 	function nextTarget() {
 		$redbullTarget = $redbullContener.children("img." + classRedbull + "." + classClosed).first();
 	};
 
 	rc.init = function() {
+		var tmp = function() {$(this).toggleClass(classFocus);};
+		$(".redbull_action").hover(tmp, tmp);
 		$redbullContener = $("#redbull_contener");
 		nextTarget();
 		$redbullClosedTemplate = $redbullTarget.clone();
